13c2cc0a39eba406c40676c6a02ed68146064407
Anton Osenenko <orangeudav@gmail.com>
2017-03-10 18:56:22 +0300

    add latency validator

--- app/models/events/statistic.rb
+++ app/models/events/statistic.rb
@@ -1,3 +1,8 @@
 class Statistic < Event
   many_to_one :ip
+
+  def validate
+    super
+    validates_numeric :latency, allow_nil: false
+  end
 end
--- config/initializers/db.rb
+++ config/initializers/db.rb
@@ -2,6 +2,7 @@ require 'sequel'
 
 Sequel.extension(:migration)
 Sequel::Model.plugin(:timestamps, update_on_create: true)
+Sequel::Model.plugin(:validation_helpers)
 ENV['DB'] ||= App.default_db
 DB = Sequel.connect(ENV['DB'])
 DB.extension(:pagination)
