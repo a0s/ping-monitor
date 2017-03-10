a7c8a67dd9ff04cefa052004190418b62f6c68d2
Anton Osenenko <orangeudav@gmail.com>
2017-03-10 20:04:30 +0300

    fix Ip creation

--- db/migrations/1489065271_create_ips.rb
+++ db/migrations/1489065271_create_ips.rb
@@ -5,7 +5,7 @@ Sequel.migration do
     create_table :ips do
       primary_key :id
       inet :ip, null: false, unique: true
-      Boolean :enabled, null: false, default: true, index: true
+      Boolean :enabled, null: false, default: false, index: true
       Time :last_checked_at, null: false, default: Time.at(0), index: true
       Time :start_checking_at, null: true, index: true
     end
